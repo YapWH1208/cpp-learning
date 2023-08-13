c410bf754b47f52713f82df44824243e88f939fe
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-08-13 15:45:04 +0800

    add comments in inter-5

--- Intermediate/Lesson/2 - Cite/5 - basic.cpp
+++ Intermediate/Lesson/2 - Cite/5 - basic.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int main(){
     int a = 10;
-    int &b = a;
+    int &b = a;// Cite a to b
 
     cout << "A = " << a << endl;
     cout << "B = " << b << endl;
 
+    // When b changes, a will also be changed
     b = 100;
 
     cout << "A = " << a << endl;
