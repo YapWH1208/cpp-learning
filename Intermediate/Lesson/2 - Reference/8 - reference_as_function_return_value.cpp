05d536a5a09ea16b6f9bddfac3dc8b8ddc5ec17d
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-08-15 11:50:08 +0800

    commit inter-8

---
+++ Intermediate/Lesson/2 - Reference/8 - reference_as_function_return_value.cpp
@@ -0,0 +1,33 @@
+# include <iostream>
+using namespace std;
+
+// Return local variable's cite
+int &test1(){
+    int a = 10;
+    return a;
+}
+
+// Return static variable's cite
+int &test2(){
+    static int a = 20;
+    return a;
+}
+
+int main(){
+    // Cannot return local variable's cite
+    int &ref = test1();
+    cout << "ref = " << ref << endl;
+    cout << "ref = " << ref << endl;
+
+    // If function is on the right side, the return value must be the cite
+    int &ref2 = test2();
+    cout << "ref2 = " << ref2 << endl;
+    cout << "ref2 = " << ref2 << endl;
+
+    test2() = 1000;
+    cout << "ref2 = " << ref2 << endl;
+    cout << "ref2 = " << ref2 << endl;
+
+    system("Pause");
+    return 0;
+}
