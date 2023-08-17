8e774e5032bb405cc6cd22e7e942c14f6ff59033
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-08-17 18:50:11 +0800

    fixes on inter-12

---
+++ Intermediate/Lesson/3 - Function Overloading/12 - function_palceholder_parameter.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+void func(int a, int){
+    cout << "This is a func" << endl;
+}
+
+int main(){
+    // Placeholder must be filled
+    func(10,10);
+
+    system("Pause");
+    return 0;
+}
